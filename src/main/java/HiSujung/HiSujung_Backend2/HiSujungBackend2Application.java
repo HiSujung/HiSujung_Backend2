63d93e0e825bf1b1490c7d4936199fdb8bcec9c5
EunJaeJo <joeunjae0925@gmail.com>
2023-07-19 15:20:49 +0900

    first commit

---
+++ src/main/java/HiSujung/HiSujung_Backend2/HiSujungBackend2Application.java
@@ -0,0 +1,13 @@
+package HiSujung.HiSujung_Backend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiSujungBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiSujungBackend2Application.class, args);
+	}
+
+}
